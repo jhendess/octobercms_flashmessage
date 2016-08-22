f4090b614c8429aafd2814e6b1d31462854f418f
jhendess <gebratene.ente+github@gmail.com>
2016-08-22 23:54:02 +0200

    Customized main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,59 +1,66 @@
-$(window).on('ajaxErrorMessage', function(event, message){
+/**
+ * Taken and adapted from https://gist.github.com/kdallas/17e07f9b50b066a13bdd
+ */
+
+$(window).on('ajaxErrorMessage', function (event, message) {
     event.preventDefault();
 });
 
-$(window).on('ajaxError', function(event, obj, context, status) {
-    var message;
-    var showfm = function(message){
-        if(document.flashflag){
-            document.romanov_flashoptions.message = message;
+$(document).ajaxError(function (event, obj, context, status) {
+    var message = '';
+    var showfm = function (message) {
+        var msgTxt = (typeof message === 'object') ? message[key][0] : message;
+
+        if (document.flashflag) {
+            document.romanov_flashoptions.message = msgTxt;
             document.romanov_flashsettings.type = 'danger';
             $.notify(document.romanov_flashoptions, document.romanov_flashsettings);
-        }else{
+        } else {
             $.notify({
-                message: message
-            },{
-                type: 'danger',
-                placement: {
-                    from: "top",
-                    align: "center"
-                },
-                delay: 10000
+                icon: 'icon-exclamation-sign',
+                message: msgTxt
+            }, {
+                type: 'danger'
             });
         }
     };
     if (typeof status.responseJSON === 'undefined') {
-        if (typeof status.responseText !== 'undefined') {
-            message = status.responseText.match(/^\"(.*)\"\s/);
-            if (message && typeof message[1] !== 'undefined') {
-                message = message[1];
-            } else {
-                message = status.responseText;
-            }
+
+        if (typeof status.responseText != 'undefined') {
+            message = status.responseText.match(/^\"(.*)\"\s/)[1];
         }
+
     } else {
         message = status.responseJSON.X_OCTOBER_ERROR_FIELDS;
     }
-    if( typeof message === 'object' ) {
-        $.each(message, function(){
-            showfm(this[0]);
-        });
-    }else if(typeof message !== 'undefined'){
+    if (typeof message === 'object') {
+        // Don't render form validations -> use formerror component instead
+    } else if (message != '') {
         showfm(message);
     }
 });
 
-$(document).ready(function(){
-    $.request('onShowFlashMsg', {
-        success: function(data){
-            if(data != ''){
-                $.each(data.msgs, function(type, msg) {
-                    var t = type == 'error' ? 'danger' : type;
-                    data.options.message = msg;
-                    data.settings.type = t;
-                    $.notify(data.options, data.settings);
-                });
-            }
+
+$(document).ajaxSuccess(function (event, obj, context, data) {
+    if (typeof data.msgs !== 'undefined') {
+        if (data != '') {
+            $.each(data.msgs, function (type, msg) {
+                data.options.message = msg;
+                var icon = '';
+                //	switch (t) {
+                switch (type) {
+                    case 'success':
+                    case 'info':
+                        icon = 'icon-ok-sign';
+                        break;
+                    case 'danger':
+                        icon = 'icon-exclamation-sign';
+                        break;
+                }
+                data.options.icon = icon;
+                data.settings.type = type;
+                $.notify(data.options, data.settings);
+            });
         }
-    });
+    }
 });
